eadde5cd456abe96c8b7646c15bb53ef15bfe718
ignacio <nachogiusti94@gmail.com>
2026-04-17 17:05:29 -0300

    Fixed imports Fixed onSearchQueryChange not working, several typos and import Removed test lines Added visual modifier

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookSearchBar.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/components/BookSearchBar.kt
@@ -1,6 +1,7 @@
 package com.plcoding.bookpedia.book.presentation.book_list.components
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -13,41 +14,42 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.ui.Modifier
 import cmp_bookpedia.composeapp.generated.resources.Res
 import org.jetbrains.compose.resources.stringResource
+import cmp_bookpedia.composeapp.generated.resources.*
 import androidx.compose.ui.graphics.Color.Companion.Blue
-import androidx.compose.ui.graphics.vector.ImageVector
 import com.plcoding.bookpedia.core.presentation.DesertWhite
-import cmp_bookpedia.composeapp.generated.resources.search_hint
 import com.plcoding.bookpedia.core.presentation.DarkBlue
 
 @Composable
 fun BookSearchBar(
     searchQuery: String,
-    searchQueryChange: (String) -> Unit,
+    onSearchQueryChange: (String) -> Unit,
     onImeSearch: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     OutlinedTextField(
         value = searchQuery,
-        onValueChange = searchQueryChange,
-        shape = RoundedCornerShape(90),
+        onValueChange = onSearchQueryChange,
+        shape = RoundedCornerShape(20),
         colors = OutlinedTextFieldDefaults.colors(
-            unfocusedBorderColor = Blue,
-            focusedBorderColor = DesertWhite,
+            unfocusedBorderColor = DarkBlue,
+            focusedBorderColor = Blue,
             cursorColor = DarkBlue,
         ),
         placeholder = {
             Text(
-                text = stringResource(Res.string.search_hint)
+                text = stringResource(Res.string.search_hint),
+                color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
             )
         },
         leadingIcon = {
             Icon(
                 imageVector = Icons.Default.Search,
                 contentDescription = null,
-                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+                tint = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.5f)
             )
         },
         singleLine = true,
@@ -62,12 +64,13 @@ fun BookSearchBar(
             ) {
                 IconButton(
                     onClick = {
-                        onSearchQueryChange()
+                        onSearchQueryChange("")
                     }
                 ) {
                     Icon(
-                        ImageVector = Icons.Default.Close,
-                        contentDescription = stringResource(Res.string.close_hint)
+                        imageVector = Icons.Default.Close,
+                        contentDescription = stringResource(Res.string.close_hint),
+                        tint = MaterialTheme.colorScheme.onSurface
                     )
                 }
             }
