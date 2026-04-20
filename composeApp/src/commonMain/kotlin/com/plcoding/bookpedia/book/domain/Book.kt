5822c8d9cc35264942eec67dd71b4c3484a3b6fa
ignacio <nachogiusti94@gmail.com>
2026-04-20 16:39:35 -0300

    Added missing values, imageUrl & firsPublishYear Corrected incorrect type of value for rating

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/Book.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/domain/Book.kt
@@ -4,9 +4,11 @@ data class Book(
     val id: String,
     val title: String,
     val authors: List<String>,
+    val imageUrl: String,
     val description: String?,
     val languages: List<String>,
-    val averageRating: Double,
+    val averageRating: Double?,
+    val firstPublishYear: Int?,
     val ratingsCount: Int?,
     val numPages: Int?,
     val numEditions: Int?
