b9da9991fa351d76625c1ac925daf3134c1f696c
ignacio <nachogiusti94@gmail.com>
2026-04-20 16:44:37 -0300

    Created code for "Search Results" and "Favorites" containers

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListScreen.kt
@@ -1,13 +1,44 @@
 package com.plcoding.bookpedia.book.presentation.book_list
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import org.koin.compose.viewmodel.koinViewModel
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.DarkGray
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.unit.dp
+import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.favorites
+import cmp_bookpedia.composeapp.generated.resources.search_results
 import com.plcoding.bookpedia.book.domain.Book
+import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
+import com.plcoding.bookpedia.core.presentation.DarkBlue
+import com.plcoding.bookpedia.core.presentation.DesertWhite
+import com.plcoding.bookpedia.core.presentation.LightBlue
+import org.jetbrains.compose.resources.stringResource
 
 @Composable
 fun BookListScreenRoot(
@@ -35,5 +66,94 @@ fun BookListScreen(
     onAction: (BookListAction) -> Unit,
     modifier: Modifier = Modifier
 ) {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(DarkBlue)
+            .statusBarsPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        BookSearchBar(
+            searchQuery = state.searchQuery,
+            onSearchQueryChange = {
+                onAction(BookListAction.OnSearchQueryChange(it))
+            },
+            onImeSearch = {
+            },
+            modifier = Modifier
+                .widthIn(max = 500.dp, min = 250.dp)
+                .fillMaxWidth()
+                .padding(16.dp),
+        )
+        Surface(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            color = Gray,
+            shape = RoundedCornerShape(
+                topStart = 24.dp,
+                topEnd = 24.dp
+                //topStartPercent = 5,
+                //topEndPercent = 5
+                )
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                TabRow(
+                    selectedTabIndex = state.selectedTabIndex,
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    containerColor = LightBlue,
+//                    indicator = { tabPosition ->
+//                        TabRowDefaults.SecondaryIndicator(
+//                            color = DesertWhite,
+//                            modifier = Modifier
+//                                .tabIndicatorOffset(tabPosition[state.selectedTabIndex])
+//                        )
+//                    }
+                ) {
+                    Tab(
+                        selected = state.selectedTabIndex == 0,
+                        onClick = {
+                            onAction(BookListAction.OnTabSelected(0))
+                        },
+                        modifier = Modifier
+                            .weight(1f),
+                        selectedContentColor = DarkBlue,
+                        unselectedContentColor = Color.DarkGray.copy(alpha = 0.85f)
+                    ) {
+                        Text(
+                            text = stringResource(Res.string.search_results),
+                            modifier = Modifier
+                                .padding(vertical = 12.dp)
+                        )
+                    }
+                    Tab(
+                        selected = state.selectedTabIndex == 1,
+                        onClick = {
+                            onAction(BookListAction.OnTabSelected(1))
+                        },
+                        modifier = Modifier
+                            .weight(1f),
+                        selectedContentColor = DarkBlue,
+                        unselectedContentColor = Color.DarkGray.copy(alpha = 0.85f)
+                    ) {
+                        Text(
+                            text = stringResource(Res.string.favorites),
+                            modifier = Modifier
+                                .padding(vertical = 12.dp)
+                        )
+                    }
+                }
+                Spacer(
+                    modifier = Modifier
+                        .height(4.dp),
+                )
+//                HorizontalPager(
+//                    state = rememberPagerState {}
+//                )
+            }
+        }
+    }
 }
