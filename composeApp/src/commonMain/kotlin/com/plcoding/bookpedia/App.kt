37858b538d74b217b435dce8a133adc2f0daba1c
ignacio <nachogiusti94@gmail.com>
2026-04-22 17:38:28 -0300

    Removed engine code to use Koin modules, platforms, etc

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/App.kt
@@ -22,20 +22,16 @@ import com.plcoding.bookpedia.book.presentation.book_list.BookListScreenRoot
 import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
 import com.plcoding.bookpedia.core.data.HttpClientFactory
 import io.ktor.client.engine.HttpClientEngine
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 @Preview
-fun App(engine: HttpClientEngine) {
+fun App() {
+    val viewModel = koinViewModel<BookListViewModel>()
     BookListScreenRoot(
-        viewModel = remember { BookListViewModel(
-            bookRepository = DefaultBookRepository(
-                remoteBookDataSource = KtorRemoteBookDataSource(
-                    httpClient = HttpClientFactory.create(engine)
-                )
-            )
-        ) },
+        viewModel = viewModel,
         onBookClick = {
-            
+
         },
     )
 }
