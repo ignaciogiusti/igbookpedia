8ca9fec2b2703bfb46b27a5c27cb5f622c812a37
ignacio <nachogiusti94@gmail.com>
2026-04-23 17:58:09 -0300

    Fixed formatting/syntax

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BookChip.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BookChip.kt
@@ -1,11 +1,14 @@
 package com.plcoding.bookpedia.book.presentation.book_detail.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.plcoding.bookpedia.core.presentation.DesertWhite
 import com.plcoding.bookpedia.core.presentation.LightBlue
@@ -28,9 +31,9 @@ fun BookChip(
                     ChipSize.REGULAR -> 80.dp
                     ChipSize.LARGE -> 100.dp
                 }
-            ),
-            .clip(RoundedCornerShape(16.dp)),
-            .background(DesertWhite),
+            )
+            .clip(RoundedCornerShape(16.dp))
+            .background(DesertWhite)
             .padding(
                 vertical = 8.dp,
                 horizontal = 12.dp
