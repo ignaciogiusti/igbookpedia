f34c4e43173403b680e9562e0eac0fa1c1c8a2e7
ignacio <nachogiusti94@gmail.com>
2026-04-23 17:23:38 -0300

    Implemented code to fetch data from selected books and display it. Removed unused imports

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/app/App.kt
@@ -1,34 +1,25 @@
 package com.plcoding.bookpedia
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.toRoute
-import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import cmp_bookpedia.composeapp.generated.resources.Res
-import cmp_bookpedia.composeapp.generated.resources.compose_multiplatform
 import com.plcoding.bookpedia.app.Route
-import com.plcoding.bookpedia.book.data.network.KtorRemoteBookDataSource
-import com.plcoding.bookpedia.book.data.repository.DefaultBookRepository
-import com.plcoding.bookpedia.book.presentation.book_list.BookListScreen
+import com.plcoding.bookpedia.book.presentation.SelectedBookViewModel
 import com.plcoding.bookpedia.book.presentation.book_list.BookListScreenRoot
 import com.plcoding.bookpedia.book.presentation.book_list.BookListViewModel
-import com.plcoding.bookpedia.core.data.HttpClientFactory
-import io.ktor.client.engine.HttpClientEngine
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
@@ -45,15 +36,28 @@ fun App() {
             ) {
                 composable<Route.BookList> {
                     val viewModel = koinViewModel<BookListViewModel>()
+                    val selectedBookViewModel =
+                        it.sharedKoinViewModel<SelectedBookViewModel>(navController)
+
+                    LaunchedEffect(true) {
+                        selectedBookViewModel.onSelectBook(null)
+                    }
+
                     BookListScreenRoot(
                         viewModel = viewModel,
-                        onBookClick = {
-
+                        onBookClick = { book ->
+                            selectedBookViewModel.onSelectBook(book)
+                            navController.navigate(
+                                Route.BookDetail(book.id)
+                            )
                         },
                     )
                 }
-                composable<Route.BookDetail> { entry ->
-                    val args = entry.toRoute<Route.BookDetail>()
+                composable<Route.BookDetail> {
+                    val selectedBookViewModel =
+                        it.sharedKoinViewModel<SelectedBookViewModel>(navController)
+                    val viewModel = koinViewModel<BookListViewModel>()
+                    val selectedBook by selectedBookViewModel.selectedBook.collectAsStateWithLifecycle()
 
 //                    Dummy box
                     Box(
@@ -61,11 +65,23 @@ fun App() {
                             .fillMaxWidth(),
                         contentAlignment = Alignment.Center
                     ) {
-                        Text("Book detail screen. Id ${args.id}")
+                        Text("$selectedBook")
                     }
                 }
             }
         }
     }
+}
 
+@Composable
+private inline fun <reified T: ViewModel> NavBackStackEntry.sharedKoinViewModel(
+    navController: NavController
+): T {
+    val navGraphRoute = destination.parent?.route ?: return koinViewModel<T>()
+    val parentEntry = remember(this) {
+        navController.getBackStackEntry(navGraphRoute)
+    }
+    return koinViewModel(
+        viewModelStoreOwner = parentEntry
+    )
 }
