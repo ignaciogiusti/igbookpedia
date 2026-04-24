8f55f6560e32f48c4a947eec5bc4da9cdf30b27c
ignacio <nachogiusti94@gmail.com>
2026-04-24 14:34:57 -0300

    Implemented code to display favorites icon with state functionality, ratings, pages and description

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/BookDetailScreen.kt
@@ -1,13 +1,45 @@
 package com.plcoding.bookpedia.book.presentation.book_detail
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.plcoding.bookpedia.app.Route
+import cmp_bookpedia.composeapp.generated.resources.Res
+import cmp_bookpedia.composeapp.generated.resources.languages
+import cmp_bookpedia.composeapp.generated.resources.no_description
+import cmp_bookpedia.composeapp.generated.resources.pages
+import cmp_bookpedia.composeapp.generated.resources.rating
+import cmp_bookpedia.composeapp.generated.resources.synopsis
 import com.plcoding.bookpedia.book.presentation.book_detail.components.BlurredImageBg
+import com.plcoding.bookpedia.book.presentation.book_detail.components.BookChip
+import com.plcoding.bookpedia.book.presentation.book_detail.components.ChipSize
+import com.plcoding.bookpedia.book.presentation.book_detail.components.TitledContent
+import com.plcoding.bookpedia.core.presentation.DesertWhite
+import org.jetbrains.compose.resources.stringResource
+import kotlin.math.round
 
 @Composable
 fun BookDetailScreenRoot(
@@ -28,6 +60,7 @@ fun BookDetailScreenRoot(
     )
 }
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 private fun BookDetailScreen(
     state: BookDetailState,
@@ -39,10 +72,126 @@ private fun BookDetailScreen(
         onBackClick = {
             onAction(BookDetailAction.OnBackClick)
         },
-        OnFavoriteClick = {
-            onAction(BookDetailAction)
+        onFavoriteClick = {
+            onAction(BookDetailAction.OnFavoriteClick)
         },
         modifier = Modifier
             .fillMaxSize()
-    ) {}
+    ) {
+        if(state.book != null) {
+            Column(
+                modifier = Modifier
+                    .widthIn(max = 700.dp)
+                    .fillMaxSize()
+                    .padding(
+                        vertical = 12.dp,
+                        horizontal = 16.dp,
+                    )
+                    .verticalScroll(rememberScrollState()),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = state.book.title,
+                    style = MaterialTheme.typography.titleSmall,
+                    textAlign = TextAlign.Center,
+                )
+                Text(
+                    text = state.book.authors.joinToString(", "),
+                    style = MaterialTheme.typography.headlineMedium,
+                    textAlign = TextAlign.Center,
+                )
+                Row(
+                    modifier = Modifier
+                        .padding(vertical = 12.dp),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    state.book.averageRating?.let { rating ->
+                        TitledContent(
+                            title = stringResource(Res.string.rating)
+                            ) {
+                            BookChip {
+                                Text(
+                                    text = "${round(rating * 10) / 10.0}",
+                                )
+                                Icon(
+                                    imageVector = Icons.Default.Star,
+                                    contentDescription = null,
+                                    tint = DesertWhite
+                                )
+                            }
+                        }
+                    }
+                    state.book.numPages?.let { pagesNum ->
+                        TitledContent(
+                            title = stringResource(Res.string.pages)
+                        ) {
+                            BookChip {
+                                Text(
+                                    text = pagesNum.toString(),
+                                )
+                            }
+                        }
+                    }
+                }
+
+                if(state.book.languages.isNotEmpty()) {
+                    TitledContent(
+                        title = stringResource(Res.string.languages),
+                        modifier = Modifier.padding(vertical = 12.dp),
+                    ) {
+                        FlowRow(
+                            horizontalArrangement = Arrangement.Center,
+                            modifier = Modifier
+                                .wrapContentSize(Alignment.Center)
+                                .fillMaxWidth(2/3f)
+                        ) {
+                            state.book.languages.forEach { availableLanguages ->
+                                BookChip(
+                                    size = ChipSize.SMALL,
+                                    modifier = Modifier.padding(2.dp),
+                                ) {
+                                    Text(
+                                        text = availableLanguages.replaceFirstChar { if (it.isLowerCase()) it.titlecase() else it.toString() },
+                                        style = MaterialTheme.typography.bodyMedium
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if(state.isLoading) {
+                    Text(
+                        text = stringResource(Res.string.synopsis),
+                        style = MaterialTheme.typography.titleLarge,
+                        modifier = Modifier
+                            .align(Alignment.Start)
+                            .fillMaxWidth()
+                            .padding(16.dp)
+                    )
+                    CircularProgressIndicator()
+//                    Box(
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .weight(1f),
+//                        contentAlignment = Alignment.Center,
+//                    ) {}
+                    Text(
+                        text = if(state.book.description.isNullOrBlank()) {
+//                            CircularProgressIndicator()
+                            stringResource(Res.string.no_description)
+                        } else {
+                            state.book.description
+                        },
+                        style = MaterialTheme.typography.bodyMedium,
+                        modifier = Modifier
+                            .align(Alignment.Start)
+                            .fillMaxWidth(3/4f),
+                        textAlign = TextAlign.Justify,
+                        color = Color.Black,
+                    )
+                }
+            }
+        }
+    }
 }
