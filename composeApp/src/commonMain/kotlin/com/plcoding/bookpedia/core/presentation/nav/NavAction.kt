bfb785194d0942affd23379e3b988b104bc14823
ignacio <nachogiusti94@gmail.com>
2026-05-08 18:23:56 -0300

    Created structure for sidebar nav

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/core/presentation/nav/NavAction.kt
@@ -0,0 +1,8 @@
+package com.plcoding.bookpedia.core.presentation.nav
+
+
+
+interface NavAction {
+//    data class OnNavClick(val nav: Nav) : NavAction
+//    data class OnButtonSelected(val index: Int ) : NavAction
+}
