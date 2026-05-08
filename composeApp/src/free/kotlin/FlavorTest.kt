03d6acd40f8db1456fb336051ea480288fffe55a
ignacio <nachogiusti94@gmail.com>
2026-05-08 14:35:04 -0300

    Created free and paid directories for flavors, files in each mainly for testing its funtcionality

---
+++ composeApp/src/free/kotlin/FlavorTest.kt
@@ -0,0 +1,13 @@
+package com.plcoding.bookpedia.core.presentation
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+actual val FlavorBg: Color = DarkOrange.copy(0.7f)
+
+@Composable
+fun FlavorText(modifier: Modifier = Modifier) {
+    Text("Free")
+}
