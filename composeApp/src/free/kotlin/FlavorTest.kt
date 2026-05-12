15dfe8df7ab7f154c488b95ddd922f8caa09e0ab
ignacio <nachogiusti94@gmail.com>
2026-05-12 13:52:36 -0300

    Created a function for flavor bg color, will be called by Androids MainActivity.kt

--- composeApp/src/free/kotlin/FlavorTest.kt
+++ composeApp/src/free/kotlin/FlavorTest.kt
@@ -1,14 +1,8 @@
 package com.plcoding.bookpedia.core.presentation
 
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import com.plcoding.bookpedia.core.presentation.FlavorBg
-
-@Composable
-fun isAndroid(): Boolean = true
 
 fun flavorBg() {
     FlavorBg = DarkOrange.copy(0.7f)
--- composeApp/src/paid/kotlin/FlavorTest.kt
+++ composeApp/src/paid/kotlin/FlavorTest.kt
@@ -3,10 +3,6 @@ package com.plcoding.bookpedia.core.presentation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-
-@Composable
-fun isAndroid(): Boolean = true
 
 fun flavorBg() {
     FlavorBg = Turquoise.copy(0.7f)
