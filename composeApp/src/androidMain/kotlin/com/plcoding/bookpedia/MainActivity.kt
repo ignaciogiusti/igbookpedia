9419f8b2b5e8c007ce43a2c7e84e6a48ea1cc359
ignacio <nachogiusti94@gmail.com>
2026-05-12 13:55:29 -0300

    Now call for the flavorBg function in FlavorTest.kt files to aplpy a a different bg color depending on Android flavor

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/MainActivity.kt
@@ -6,11 +6,13 @@ import androidx.activity.compose.setContent
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
 import com.plcoding.bookpedia.app.App
+import com.plcoding.bookpedia.core.presentation.flavorBg
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        flavorBg()
         setContent {
             App()
         }
