0f1b25a1ae547274cb065d17a3c07c75b0d147a5
ignacio <nachogiusti94@gmail.com>
2026-05-13 11:41:58 -0300

    Commented plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
     // Make sure that you have the Google services Gradle plugin dependency
     id("com.google.gms.google-services") version "4.4.4" apply false
 
-//    id("com.android.application") version "7.3.0" apply false
+//    id("com.android.application") version "9.1.1" apply false
 
     // Add the dependency for the App Distribution Gradle plugin
     id("com.google.firebase.appdistribution") version "5.2.1" apply false
